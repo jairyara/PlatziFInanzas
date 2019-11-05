1851a9c0b93331fb71196bcea3e74c231118f4a3
Jair Yara <jair.yara11@gmail.com>
2019-11-05 00:04:03 -0500

    Fix code

---
+++ PlatziFinanzas/Application/Main/Main/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlatziFinanzas
+//
+//  Created by Macbook on 4/11/19.
+//  Copyright © 2019 Oneago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
